25a51efdff57032b50ca0aea1e8edbc36a360044
ADI-m-tech <179952634+ADI-m-tech@users.noreply.github.com>
2026-07-01 14:15:50 +0530

    auth working

---
+++ Backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "MindCare backend is running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+
+export default app;
